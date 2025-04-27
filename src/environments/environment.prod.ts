e6c5d8a097e2d7727bd99de6d4b860fad86efa0c
tlavrova <tlavrova230@gmail.com>
2025-04-27 13:18:49 +0200

    Update asset paths and API URLs in environment configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/item-selector-rentman/response.json'
+  apiUrl: 'response.json'
 }; 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: '/response.json'
+  apiUrl: 'response.json'
 }; 
